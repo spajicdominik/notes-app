97db527483b1561e15c73bdf95049bd78ee1550f
spajicdominik <dominikspajic7@gmail.com>
2024-09-08 23:39:23 +0200

    added delete button

--- application/src/components/PostsList.jsx
+++ application/src/components/PostsList.jsx
@@ -1,12 +1,26 @@
-import classes from "./PostsList.module.css";
-import Post from "./Post";
-import { useState } from "react";
+import React from 'react';
+import Post from './Post';
+import { BsFillTrashFill } from "react-icons/bs";
+import classes from './PostsList.module.css';
 
-function PostsList({name, author, postsArray}) {
+function PostsList({ postsArray }) {
+  const handleDelete = (id) => {
+    console.log('Delete post with id:', id);
+    // Add logic to delete post
+  };
+
   return (
-      <div className="col-md-6 col-12">
-        {postsArray.map((post) => <Post author={post.author} body={post.body} key={post.body}></Post>)}
-      </div>
+    <div className="col-md-6 col-12">
+      {postsArray.map((post) => (
+        <div className={classes.postContainer} key={post._id}>
+          <Post author={post.author} body={post.body} />
+          <BsFillTrashFill 
+            className={classes.trashIcon} 
+            onClick={() => handleDelete(post._id)} 
+          />
+        </div>
+      ))}
+    </div>
   );
 }
 
