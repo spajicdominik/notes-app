89d216c35d2811b15674d74c6ee221dbdbf572b3
spajicdominik <dominikspajic7@gmail.com>
2024-09-09 14:19:37 +0200

    Fix port for Heroku deployment

--- app.js
+++ app.js
@@ -158,7 +158,7 @@ io.on('connection', (socket) => {
 });
 
 
-
-server.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
   console.log("HTTP/Socket.IO Server is running on http://localhost:8080");
 });
